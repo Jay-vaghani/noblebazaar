d6828527ca7d2a17925bd2aece4f29046f59f051
JayVaghani-Dev <jayvaghanidev@gmail.com>
2023-09-09 19:35:57 +0530

    cookie last try3

--- app.js
+++ app.js
@@ -25,10 +25,12 @@ app.use(
 // ******************** ROUTES ********************
 app.get("/", (req, res, next) => {
   res
-    .cookie("ok", "done", {
+    .cookie("ok", "done 2", {
       secure: true,
       sameSite: "none",
-      secure: true,
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
     })
     .status(200)
     .json({
--- utils/sendCookie.js
+++ utils/sendCookie.js
@@ -1,13 +1,11 @@
 const jwt = require("jsonwebtoken");
 
-module.exports.sendCookies = (res, user, message, statusCode = 200) => {
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRETE, {
+module.exports.sendCookies = async (res, user, message, statusCode = 200) => {
+  const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRETE, {
     expiresIn: process.env.JWT_EXPIRE,
   });
 
   const cookieOptions = {
-    httpOnly: true,
-    secure: true,
     sameSite: "none",
     secure: true,
     expires: new Date(
